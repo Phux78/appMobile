9caa4c1f82233ebac35aaceaf6f400b1d0529d89
KyosaixZ <kyosaix@hotmail.com>
2022-11-05 19:16:16 +0700

    alluser to feed
    alluser is suck no need

--- App.js
+++ App.js
@@ -3,9 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import ViewAllUser from './components/ViewAllUsers';
+import Feed from './components/Feed';
 import HomeScreen from './components/HomeScreen';
-import Feeds from './components/Feeds';
 import FreelanceProfile from './components/FreelanceProfile';
 
 const Drawer = createDrawerNavigator();
@@ -14,31 +13,21 @@ const App = () => {
   return (
     <NavigationContainer style={styles.container}>
       <Drawer.Navigator>
-
+      <Drawer.Screen
+          name="Feed"
+          component={Feed}
+          options={{ drawerLabel: 'Feed' }}
+        />
         <Drawer.Screen
           name="Home"
           component={HomeScreen}
           options={{ drawerLabel: 'Home' }}
         />
-
-        <Drawer.Screen
-          name="Feeds"
-          component={Feeds}
-          options={{ drawerLabel: 'Feeds' }}
-        />
-
-        <Drawer.Screen
-          name="ViewAllUser"
-          component={ViewAllUser}
-          options={{ drawerLabel: 'View User' }}
-        />
-
         <Drawer.Screen
           name="FreelanceProfile"
           component={FreelanceProfile}
-          options={{ drawerLabel: 'FreelanceProfile' }}
+          options={{ drawerItemStyle: { height: 0 } }}
         />
-
       </Drawer.Navigator>    
     </NavigationContainer>
   );
